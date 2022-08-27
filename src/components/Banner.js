bd5878e66b39122e1f373399b6b6d310b04635b2
Ian Costa <ianndcg@gmail.com>
2022-08-27 11:07:14 -0300

    Banner.js finished

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,9 +7,9 @@ import headerImg from '../assets/img/header-img.svg';
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ['Web developer', 'Web Designer', 'UI/UX Designer'];
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const toRotate = ['Web Developer', 'Web Designer', 'UI/UX Designer'];
   const period = 2000;
 
   useEffect(() => {
@@ -25,6 +25,24 @@ export const Banner = () => {
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
   };
 
   return (
@@ -35,7 +53,7 @@ export const Banner = () => {
             <span className='tagline'>Welcome to my Portfolio</span>
             <h1>
               {`Hi I'm Ian `}
-              <span className='wrap'>web developer</span>
+              <span className='wrap'>{text}</span>
               <p>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Sapiente natus voluptates quas eveniet officia itaque vero
@@ -48,7 +66,7 @@ export const Banner = () => {
             </h1>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt='Header Image' />
+            <img src={headerImg} alt='Header Img' />
           </Col>
         </Row>
       </Container>
